ce861ee27bbd48b8b11d4af57be21fa938fba408
Sean Worrell <spworrell@gmail.com>
2023-08-24 22:27:18 -0400

    qs made

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = require('inquirer');
+const markupMaker = require('./utils/generateMarkdown.js');
+
+
+questions
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project name?',
+      name: 'projectName',
+    },
+    {
+      type: 'input',
+      message: 'Enter the installation instructions:',
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter the usage information:',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Do you have any credits to add?',
+        name: 'credits',
+      },
+      {
+        type: 'list',
+        message: 'Please selecte the type of license:',
+        choices: ['MIT', 'GNU GPL v3', 'Mozilla Public License 2.0'],
+        name: 'license',
+      },
+
+  ])
+  .then((answers) =>
+  markupMaker.answers(answers))
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
